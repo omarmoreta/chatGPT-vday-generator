cc0df7f50372364912baba5f0149a101023441f6
Omar <o.moreta@gmail.com>
2023-02-11 06:56:57 -0500

    created endpoint for api request and view for generator

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,45 @@
+import React from "react";
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [gender, setGender] = useState("woman");
+  const [age, setAge] = useState("25");
+  const [priceMin, setPriceMin] = useState("25");
+  const [priceMax, setPriceMax] = useState("100");
+  const [hobbies, setHobbies] = useState("");
   const [result, setResult] = useState();
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
+    if (loading) {
+      return;
+    }
+    setLoading(true);
     try {
-      const response = await fetch("/api/generate", {
+      const response = await fetch("/api/generateGifts", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ priceMin, priceMax, age, gender, hobbies }),
       });
-
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
-
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+      setResult(data.result.replaceAll("\n", "<br>"));
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -39,19 +51,72 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        {/* <img src="/dog.png" className={styles.icon} /> */}
+        <h3>Valentine's Day gift generator 🎁 💡</h3>
         <form onSubmit={onSubmit}>
+          <label>For who is the gift?</label>
+          <select
+            name="gender"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <option value="man">Man</option>
+            <option value="woman">Woman</option>
+          </select>
+
+          <label>Age</label>
+          <input
+            type="number"
+            min={1}
+            max={99}
+            name="age"
+            placeholder="Enter the age"
+            value={age}
+            onChange={(e) => setAge(Number.parseInt(e.target.value))}
+          />
+
+          <label>Price from</label>
+          <input
+            type="number"
+            min={1}
+            name="priceMin"
+            placeholder="Enter the minimum price"
+            value={priceMin}
+            onChange={(e) => setPriceMin(Number.parseInt(e.target.value))}
+          />
+
+          <label>Price to</label>
+          <input
+            type="number"
+            min={1}
+            name="priceMax"
+            placeholder="Enter the maximum price"
+            value={priceMax}
+            onChange={(e) => setPriceMax(Number.parseInt(e.target.value))}
+          />
+
+          <label>Hobbies</label>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="hobbies"
+            placeholder="Enter the hobbies"
+            value={hobbies}
+            onChange={(e) => setHobbies(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate gift ideas" />
         </form>
-        <div className={styles.result}>{result}</div>
+        {loading && (
+          <div>
+            <h3>Looking for the best gift ideas 🎁 💡</h3>
+            <img src="/loading.webp" className={styles.loading} />
+          </div>
+        )}
+        {result && (
+          <div
+            className={styles.result}
+            dangerouslySetInnerHTML={{ __html: result }}
+          />
+        )}
       </main>
     </div>
   );
